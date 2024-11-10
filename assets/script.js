54c6229876cfe6e5e84f3cf0c6551afa08831b2a
devbrasilbr <devbrasilbr@gmail.com>
2024-11-10 11:03:03 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#menu');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+    menu.classList.toggle('menu-desactive');
+    menu.classList.toggle('menu-active');
+}
+
+// menu.onclick=function(){
+    // toggleMenu()
+    // alert('ok')
+// }
+
+// toggleMenu()
